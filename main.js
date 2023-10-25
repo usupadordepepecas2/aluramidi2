c90dba726d5d8e731ce5beef98a84e16af68955e
matheus <108078152+usupadordepepecas2@users.noreply.github.com>
2023-10-25 17:49:11 -0300

    gj

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-document.querySelector('.tecla_som') 
+function tocaSomPom{
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_som').onclick=tocaSomPom
 function tocarSomPom(){
     document.querySelector('tocar_tecla_pom').play();
 }
