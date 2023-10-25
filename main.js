9795e4dde03264e7c692ef4fcc8ddcd23ca3ed40
matheus <108078152+usupadordepepecas2@users.noreply.github.com>
2023-10-25 17:50:02 -0300

    fh

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function tocaSomPom{
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_som').onclick=tocaSomPom
+document.querySelector('.tecla_som').onclick=tocaSomPom;
 function tocarSomPom(){
     document.querySelector('tocar_tecla_pom').play();
 }
