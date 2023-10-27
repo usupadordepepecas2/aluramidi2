abd5c0d591a30603bad2d65b3b5155e0a66cc131
matheus <108078152+usupadordepepecas2@users.noreply.github.com>
2023-10-27 16:23:52 -0300

    dg

--- main.js
+++ main.js
@@ -20,11 +20,18 @@ function tocaSomPuff() {
     document.querySelector('#som_tecla_puff').play();
 }
 
+
 document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+
+
 
 function tocaSomSplash() {
     document.querySelector('#som_tecla_splash').play();
 }
+
+
+
 
 document.querySelector('.tecla_splash').onclick = tocaSomSplash;
 
@@ -32,11 +39,18 @@ function tocaSomToim() {
     document.querySelector('#som_tecla_toim').play();
 }
 
+
+
+
 document.querySelector('.tecla_toim').onclick = tocaSomToim;
 
 function tocaSomPsh() {
     document.querySelector('#som_tecla_psh').play();
 }
+
+
+
+
 
 document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 
@@ -44,19 +58,28 @@ function tocaSomToim() {
     document.querySelector('#som_tecla_toim').play();
 }
 
+
+
+
+
 document.querySelector('.tecla_toim').onclick = tocaSomToim;
 
 function tocaSomPsh() {
     document.querySelector('#som_tecla_psh').play();
 }
 
+
+
+
 document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 
 function tocaSomTic() {
     document.querySelector('#som_tecla_tic').play();
 }
-
 document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+
+
 
 function tocaSomTom() {
     document.querySelector('#som_tecla_tom').play();
