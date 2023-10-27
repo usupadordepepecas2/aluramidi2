4aa45c469c3abcfc9180382e2fd3470b58b10cf6
matheus <108078152+usupadordepepecas2@users.noreply.github.com>
2023-10-27 16:03:58 -0300

    gdg

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ function tocaSomClap() {
 
 document.querySelector('.tocaSomClap').onclick = tocaSomClap;
 
+function tocaSomTim() {
+    document.querySelector('#tocaSomTim').play();
+}
+
+document.querySelector('.tocaSomTim').onclick = tocaSomTim;
